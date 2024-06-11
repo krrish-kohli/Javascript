1b76fadd83a678b441668ee2393c8107a97f4d06
krrish-kohli <krrishkohli@icloud.com>
2024-06-10 22:18:23 -0700

    first commit

---
+++ JS Practice Assignments/Assignment1b.js
@@ -0,0 +1,6 @@
+
+    /*
+    This is the first assignment in which we have to write our name using javascript(internal script).
+    Using this code I entered my name 
+    */
+    document.write("My name is Krrish Kohli.")
